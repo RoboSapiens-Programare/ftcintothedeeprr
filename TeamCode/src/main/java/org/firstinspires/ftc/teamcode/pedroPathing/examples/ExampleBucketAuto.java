2f2d3e47df5135a5aef36911e3787b6dc36014f0
calin <swiorx@gmail.com>
2024-12-03 20:21:59 +0200

    bucket autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/ExampleBucketAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/ExampleBucketAuto.java
@@ -9,7 +9,6 @@ import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
@@ -26,9 +25,9 @@ import org.firstinspires.ftc.teamcode.robot.UniversalValues;
  * @version 2.0, 11/28/2024
  */
 
-@Autonomous(name = "Example Auto Blue", group = "Examples")
+@Autonomous(name = "Auto Bucket", group = "Examples")
 public class ExampleBucketAuto extends OpMode {
-
+    private Robot robot = null;
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
 
@@ -47,84 +46,198 @@ public class ExampleBucketAuto extends OpMode {
      * This visualizer is very easy to use to find and create paths/pathchains/poses: <https://pedro-path-generator.vercel.app/>
      * Lets assume our robot is 18 by 18 inches
      * Lets assume the Robot is facing the human player and we want to score in the bucket */
+    private final Pose startPose = new Pose(8,103, Math.toRadians(-90));
+    private final Pose scorePose = new Pose(20,123, Math.toRadians(315));
+    private final Pose pickup1Pose = new Pose(24, 120, Math.toRadians(0));
+    private final Pose pickup2Pose = new Pose(24, 130, Math.toRadians(0));
+    private final Pose pickup3Pose = new Pose(45, 121, Math.toRadians(90));
+    private final Pose parkPose = new Pose(59,98, Math.toRadians(-90));
+    private final Pose parkEmergencyPose = new Pose(14,29, Math.toRadians(-90));
 
-    /** Start Pose of our robot */
-    private final Pose startPose = new Pose(9, 111, Math.toRadians(0));
 
-    /** Scoring Pose of our robot. It is facing the submersible at a -45 degree (315 degree) angle. */
-    private final Pose scorePose = new Pose(14, 129, Math.toRadians(315));
-
-    /** Lowest (First) Sample from the Spike Mark */
-    private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0));
-
-    /** Middle (Second) Sample from the Spike Mark */
-    private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0));
-
-    /** Highest (Third) Sample from the Spike Mark */
-    private final Pose pickup3Pose = new Pose(49, 135, Math.toRadians(0));
-
-    /** Park Pose for our robot, after we do all of the scoring. */
-    private final Pose parkPose = new Pose(60, 98, Math.toRadians(90));
-
-    /** Park Control Pose for our robot, this is used to manipulate the bezier curve that we will create for the parking.
-     * The Robot will not go to this pose, it is used a control point for our bezier curve. */
-    private final Pose parkControlPose = new Pose(60, 98, Math.toRadians(90));
-
-    /* These are our Paths and PathChains that we will define in buildPaths() */
-    private Path scorePreload, park;
-    private PathChain pula;
-
+    private Path scorePreload;
+    private PathChain grabPickup1, grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3, park;
     /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
      * It is necessary to do this so that all the paths are built before the auto starts. **/
     public void buildPaths() {
+        //score preload path
+    scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose)));
+    scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
 
-        /* There are two major types of paths components: BezierCurves and BezierLines.
-         *    * BezierCurves are curved, and require >= 3 points. There are the start and end points, and the control points.
-         *    - Control points manipulate the curve between the start and end points.
-         *    - A good visualizer for this is [this](https://pedro-path-generator.vercel.app/).
-         *    * BezierLines are straight, and require 2 points. There are the start and end points.
-         * Paths have can have heading interpolation: Constant, Linear, or Tangential
-         *    * Linear heading interpolation:
-         *    - Pedro will slowly change the heading of the robot from the startHeading to the endHeading over the course of the entire path.
-         *    * Constant Heading Interpolation:
-         *    - Pedro will maintain one heading throughout the entire path.
-         *    * Tangential Heading Interpolation:
-         *    - Pedro will follows the angle of the path such that the robot is always driving forward when it follows the path.
-         * PathChains hold Path(s) within it and are able to hold their end point, meaning that they will holdPoint until another path is followed.
-         * Here is a explanation of the difference between Paths and PathChains <https://pedropathing.com/commonissues/pathtopathchain.html> */
+        //grab pickup 1 path
+    grabPickup1 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(scorePose), new Point(pickup1Pose)))
+            .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+            .build();
 
-        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
-        pula = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(9.000, 111.000, Point.CARTESIAN), new Point(48.000, 111.000, Point.CARTESIAN)))
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(315))
-                .addPath(
-                        new BezierLine(
-                                new Point(48.000, 111.000, Point.CARTESIAN),
-                                new Point(30.000, 54.000, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(315), Math.toRadians(0))
-                .addPath(
-                        // Line 3
-                        new BezierLine(
-                                new Point(30.000, 54.000, Point.CARTESIAN),
-                                new Point(111.000, 23.000, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(190))
-                .build();
+        //score pickup 1 path
+    scorePickup1 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(pickup1Pose), new Point(scorePose)))
+            .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+            .build();
 
+        //grab pickup 2 path
+    grabPickup2 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(scorePose), new Point(pickup2Pose)))
+            .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Pose.getHeading())
+            .build();
+
+        //score pickup 2 path
+    scorePickup2 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(pickup2Pose), new Point(scorePose)))
+            .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
+            .build();
+
+        //grab pickup 3 path
+    grabPickup3 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(scorePose), new Point(pickup3Pose)))
+            .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Pose.getHeading())
+            .build();
+
+        //score pickup 3 path
+    scorePickup3 = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(pickup3Pose), new Point(scorePose)))
+            .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
+            .build();
+
+        //park path
+    park = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(scorePose), new Point(parkPose)))
+            .setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading())
+            .build();
     }
+
+
 
     /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
      * Everytime the switch changes case, it will reset the timer. (This is because of the setPathState() method)
      * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
+    public void autonomousPathUpdate() {
+        switch (pathState) {
 
+            //GOES TO SCORE POSITION
+            case 0:
+                follower.followPath(scorePreload);
+                setPathState(1);
+                break;
+
+            //CHECKS IF ROBOT IS AT SCORE POSITION, SCORES PRELOAD, GOES TO GRABBING FIRST SAMPLE POSITION AND HOLDS
+            case 1:
+                /* You could check for
+                - Follower State: "if(!follower.isBusy() {}" (Though, I don't recommend this because it might not return due to holdEnd
+                - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
+                - Robot Position: "if(follower.getPose().getX() > 36) {}"
+                */
+                if(opmodeTimer.getElapsedTimeSeconds() > 25){
+                    setPathState(9);
+                }
+                if(follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    robot.intake.OpenIntake(UniversalValues.CLAW_OPEN);
+                    if(actionTimer.getElapsedTimeSeconds() > 1){
+                        robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                        follower.followPath(grabPickup1, /* holdEnd = */ true);
+                        actionTimer.resetTimer();
+                        setPathState(2);
+                    }
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT GRAB POSITION, GRABS SAMPLE, GOES TO SCORING POSITION AND HOLDS
+            case 2:
+                if(follower.getPose().getX() > (pickup1Pose.getX() - 1) && follower.getPose().getY() > (pickup1Pose.getY() - 1)) {
+                    robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                    follower.followPath(scorePickup1, /* holdEnd = */ true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+
+            //CHECKS IF ROBOT IS AT SCORE POSITION, SCORES SAMPLE, GOES TO GRABBING SECOND SAMPLE POSITION AND HOLDS
+                if(follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    robot.intake.OpenIntake(UniversalValues.CLAW_OPEN);
+                    if(actionTimer.getElapsedTimeSeconds() > 1){
+                        robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                        follower.followPath(grabPickup2, /* holdEnd = */ true);
+                        actionTimer.resetTimer();
+                        setPathState(4);
+                    }
+
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT GRAB POSITION, GRABS SAMPLE, GOES TO SCORING POSITION AND HOLDS
+            case 4:
+                if(follower.getPose().getX() > (pickup2Pose.getX() - 1) && follower.getPose().getY() > (pickup2Pose.getY() - 1)) {
+                    robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                    follower.followPath(scorePickup2, /* holdEnd = */ true);
+                    setPathState(5);
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT SCORE POSITION, SCORES SAMPLE, GOES TO GRABBING THIRD SAMPLE POSITION AND HOLDS
+            case 5:
+                if(follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    robot.intake.OpenIntake(UniversalValues.CLAW_OPEN);
+                    if(actionTimer.getElapsedTimeSeconds() > 1){
+                        robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                        follower.followPath(grabPickup3, /* holdEnd = */ true);
+                        actionTimer.resetTimer();
+                        setPathState(6);
+                    }
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT GRAB POSITION, GRABS SAMPLE, GOES TO SCORING POSITION AND HOLDS
+            case 6:
+                if(follower.getPose().getX() > (pickup3Pose.getX() - 1) && follower.getPose().getY() > (pickup3Pose.getY() - 1)) {
+                    robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                    follower.followPath(scorePickup3, /* holdEnd = */ true);
+                    setPathState(7);
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT SCORE POSITION, SCORES SAMPLE, GOES TO PARKING POSITION AND HOLDS
+            case 7:
+                if(follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    robot.intake.OpenIntake(UniversalValues.CLAW_OPEN);
+                    if(actionTimer.getElapsedTimeSeconds() > 1){
+                        robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                        follower.followPath(park, /* holdEnd = */ true);
+                        actionTimer.resetTimer();
+                        setPathState(8);
+                    }
+                }
+                break;
+
+            //CHECKS IF ROBOT IS AT PARKING POSITION, EXTEND SLIDES AT LEVEL 1 ASCENT POSITION
+            case 8:
+                if(follower.getPose().getX() > (parkPose.getX() - 1) && follower.getPose().getY() > (parkPose.getY() - 1)) {
+                    robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+                    setPathState(-1);
+                }
+                break;
+
+            //IF ANYTHING GOES WRONG THEN EMERGENCY PARK
+            case 9:
+                follower.breakFollowing();
+                PathChain parkEmergency;
+                parkEmergency = follower.pathBuilder()
+                        .addPath(new BezierLine(new Point(follower.getPose()), new Point(parkEmergencyPose)))
+                        .setLinearHeadingInterpolation(follower.getPose().getHeading(), parkEmergencyPose.getHeading())
+                        .build();
+                follower.followPath(parkEmergency, /* holdEnd = */ true);
+                break;
+        }
+
+        }
 
 
     /** These change the states of the paths and actions
      * It will also reset the timers of the individual switches **/
-
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
 
     /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
     @Override
@@ -132,7 +245,7 @@ public class ExampleBucketAuto extends OpMode {
 
         // These loop the movements of the robot
         follower.update();
-
+        autonomousPathUpdate();
 
         // Feedback to Driver Hub
         telemetry.addData("path state", pathState);
@@ -145,10 +258,9 @@ public class ExampleBucketAuto extends OpMode {
     /** This method is called once at the init of the OpMode. **/
     @Override
     public void init() {
-
+        robot = new Robot(hardwareMap);
         pathTimer = new Timer();
         opmodeTimer = new Timer();
-
         opmodeTimer.resetTimer();
 
         follower = new Follower(hardwareMap);
@@ -157,20 +269,27 @@ public class ExampleBucketAuto extends OpMode {
         buildPaths();
 
 
-
         // Set the claw to positions for init
+        robot.intake.ManualLevel(0,1);
+        robot.intake.CloseIntake(UniversalValues.CLAW_CLOSE);
+        robot.intake.setClawPivot(UniversalValues.CLAW_HORIZONTAL);
+        robot.intake.setPivot(UniversalValues.INTAKE_INIT);
+        robot.outtake.setPivot(UniversalValues.OUTTAKE_COLLECT);
+        robot.outtake.CloseOuttake(UniversalValues.OUTTAKE_CLOSE);
     }
 
     /** This method is called continuously after Init while waiting for "play". **/
     @Override
-    public void init_loop() {}
+    public void init_loop() {
+        opmodeTimer.resetTimer();
+    }
 
     /** This method is called once at the start of the OpMode.
      * It runs all the setup actions, including building paths and starting the path system **/
     @Override
     public void start() {
         opmodeTimer.resetTimer();
-        follower.followPath(pula, true);
+        setPathState(0);
     }
 
     /** We do not use this because everything should automatically disable **/
