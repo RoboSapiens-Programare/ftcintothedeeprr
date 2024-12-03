432df8be87d62eb993972f5a17f2a31e612d508d
Robo-Sapiens-Victus <robosapienscnshb@gmail.com>
2024-12-03 17:30:36 +0200

    pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/follower/Follower.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/follower/Follower.java
@@ -170,6 +170,8 @@ public class Follower {
         // TODO: Make sure that this is the direction your motors need to be reversed in.
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         motors = Arrays.asList(leftFront, leftRear, rightFront, rightRear);
 
