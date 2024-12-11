62a1671c9822fdef255f33821efad40f26ea4339
stefan <stefan.adrian.elefterescu@gmail.com>
2024-12-11 15:39:12 +0200

    made ParkSpecimen use states
    Signed-off-by: Robosapiens <robosapienscnshb@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ParkSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ParkSpecimen.java
@@ -17,65 +17,117 @@ import org.firstinspires.ftc.teamcode.robot.subsystems.universalValues;
 public class ParkSpecimen extends OpMode {
     private org.firstinspires.ftc.teamcode.robot.subsystems.robot robot = null;
     private Follower follower;
-    private int pathState;
-    private final Pose startPose = new Pose(8,49, Math.toRadians(0)); // visualizer: (x: 10 y: 46)
+
+    private enum PathState {
+        PATH_START, SCORE_FIRST_SAMPLE, GRAB_SECOND_SAMPLE, SCORE_SECOND_SAMPLE,
+        GRAB_THIRD_SAMPLE, SCORE_THIRD_SAMPLE,
+        PATH_END
+    }
+    private PathState pathState;
+    private final Pose startPose = new Pose(8,63, Math.toRadians(0)); // visualizer: (x: 10 y: 46)
+    private final Pose firstSampleGrabPose = new Pose(70, 28, Math.toRadians(0));
+    private final Pose firstSampleScorePose = new Pose(8, 28, Math.toRadians(0));
+    private final Pose secondSampleGrabPose = new Pose(70, 17, Math.toRadians(0));
+    private final Pose secondSampleScorePose = new Pose(8, 17, Math.toRadians(0));
+    private final Pose thirdSampleGrabPose = new Pose(70, 12, Math.toRadians(0));
+    private final Pose thirdSampleScorePose = new Pose(8, 12, Math.toRadians(0));
+    // private final Pose
     private boolean singleton = true;
-    private PathChain samplePush;
+    private PathChain firstSampleGrab, secondSampleGrab, thirdSampleGrab, firstSampleScore, secondSampleScore, thirdSampleScore;
 
-    private void generatePaths() {
-        samplePush = follower.pathBuilder()
 
+    private void buildPaths() {
+        firstSampleGrab = follower.pathBuilder()
                 .addPath(
-                        // Line 1
                         new BezierCurve(
-                                new Point(8.000, 63.000, Point.CARTESIAN),
+                                new Point(startPose),
                                 new Point(16.168, 24.757, Point.CARTESIAN),
                                 new Point(45.103, 50.551, Point.CARTESIAN),
-                                new Point(70.000, 28.000, Point.CARTESIAN)
+                                new Point(firstSampleGrabPose)
                         )
-                )
-//                .setConstantHeadingInterpolation(Math.toRadians(0))
+                ).build();
+        firstSampleScore = follower.pathBuilder()
                 .addPath(
-                        // Line 2
                         new BezierLine(
-                                new Point(70.000, 28.000, Point.CARTESIAN),
-                                new Point(8.000, 28.000, Point.CARTESIAN)
+                                new Point(firstSampleGrabPose),
+                                new Point(firstSampleScorePose)
                         )
                 )
+                .build();
+
+        secondSampleGrab = follower.pathBuilder()
                 .addPath(
-                        // Line 3
                         new BezierCurve(
-                                new Point(8.000, 28.000, Point.CARTESIAN),
+                                new Point(firstSampleScorePose),
                                 new Point(61.925, 32.159, Point.CARTESIAN),
-                                new Point(70.000, 17.000, Point.CARTESIAN)
+                                new Point(secondSampleGrabPose)
                         )
                 )
+                .build();
+        secondSampleScore = follower.pathBuilder()
                 .addPath(
-                        // Line 4
                         new BezierLine(
-                                new Point(70.000, 17.000, Point.CARTESIAN),
-                                new Point(8.000, 17.000, Point.CARTESIAN)
+                                new Point(secondSampleGrabPose),
+                                new Point(secondSampleScorePose)
                         )
                 )
+                .build();
+
+        thirdSampleGrab = follower.pathBuilder()
                 .addPath(
-                        // Line 5
                         new BezierCurve(
-                                new Point(8.000, 17.000, Point.CARTESIAN),
+                                new Point(secondSampleScorePose),
                                 new Point(75.832, 22.065, Point.CARTESIAN),
-                                new Point(70.000, 12.000, Point.CARTESIAN)
+                                new Point(thirdSampleGrabPose)
                         )
                 )
+                .build();
+        thirdSampleScore = follower.pathBuilder()
                 .addPath(
-                        // Line 6
                         new BezierLine(
-                                new Point(70.000, 12.000, Point.CARTESIAN),
-                                new Point(8.000, 12.000, Point.CARTESIAN)
+                                new Point(thirdSampleGrabPose),
+                                new Point(thirdSampleScorePose)
                         )
                 )
                 .build();
     }
 
+    private void setPathState(PathState state) {
+        pathState = state;
+    }
 
+    private void autonomousPathUpdate() {
+        switch (pathState) {
+            case PATH_START:
+                follower.followPath(firstSampleGrab, true);
+                setPathState(PathState.SCORE_FIRST_SAMPLE);
+                break;
+            case SCORE_FIRST_SAMPLE:
+                follower.followPath(firstSampleScore);
+                setPathState(PathState.GRAB_SECOND_SAMPLE);
+                break;
+            case GRAB_SECOND_SAMPLE:
+                follower.followPath(secondSampleGrab);
+                setPathState(PathState.SCORE_SECOND_SAMPLE);
+                break;
+            case SCORE_SECOND_SAMPLE:
+                follower.followPath(secondSampleScore);
+                setPathState(PathState.GRAB_THIRD_SAMPLE);
+                break;
+            case GRAB_THIRD_SAMPLE:
+                follower.followPath(thirdSampleGrab);
+                setPathState(PathState.SCORE_THIRD_SAMPLE);
+                break;
+            case SCORE_THIRD_SAMPLE:
+                follower.followPath(thirdSampleScore);
+                setPathState(PathState.PATH_END);
+                break;
+            case PATH_END:
+                // maybe park robot somewhere else
+                break;
+        }
+
+    }
 
 
     @Override
@@ -88,8 +140,8 @@ public class ParkSpecimen extends OpMode {
 
         telemetry.update();
 
-        // buildPaths();
-        generatePaths();
+        buildPaths();
+
 
         // Set the subsystems to positions for init
         robot.intake.ManualLevel(0,1);
@@ -100,13 +152,19 @@ public class ParkSpecimen extends OpMode {
         robot.outtake.CloseOuttake(universalValues.OUTTAKE_CLOSE);
     }
 
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
     @Override
     public void loop() {
-        if (singleton) {
-            follower.followPath(samplePush);
-            singleton = false;
-        }
 
+        // These loop the movements of the robot
         follower.update();
+        autonomousPathUpdate();
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
     }
 }
