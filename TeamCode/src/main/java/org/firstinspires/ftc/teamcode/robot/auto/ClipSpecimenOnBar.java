3ea64bd7cbd40253f408251b55b74ea9fc655091
serb <74992183+minusonee@users.noreply.github.com>
2024-12-12 22:30:10 +0200

    added sample specimen clipon autonom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ClipSpecimenOnBar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ClipSpecimenOnBar.java
@@ -22,74 +22,99 @@ public class ClipSpecimenOnBar extends OpMode {
 
     private org.firstinspires.ftc.teamcode.robot.subsystems.robot robot = null;
     private Follower follower;
-    private Timer underbartimer, clipontimer;
+    private Timer stateTimer, pathTimer;
     private boolean singleton = true;
-
-    public void specimenonbar()
-    {
-
-        if (gamepad1.cross)
-        {
-            robot.outtake.setPivot(universalValues.OUTTAKE_CLIPON_UP);
-        }
-
-        if (gamepad1.square)
-        {
-            robot.outtake.setPivot(universalValues.OUTTAKE_CLIPON_DOWN);
-        }
-
-        if (gamepad1.circle)
-        {
-            robot.outtake.setPivot(universalValues.OUTTAKE_COLLECT);
-        }
-
-        if (gamepad1.left_bumper)
-        {
-            robot.outtake.OpenOuttake(universalValues.OUTTAKE_CLOSE);
-        }
-
-        if (gamepad1.right_bumper)
-        {
-            robot.outtake.OpenOuttake(universalValues.OUTTAKE_OPEN);
-        }
-
-    }
+    private int pathState;
 
     private final Pose startPose = new Pose(10.7,49, Math.toRadians(0));
-    private final Pose barCliponPose = new Pose(37.5,69.5, Math.toRadians(0));
+    private final Pose barCliponPose = new Pose(37.5,69.5, Math.toRadians(180));
     private PathChain toBar1;
 
     public void buildPaths() {
+        // TODO : fix Linear Heading Interpolation for rotation to the right without hard coding value bigger than 180 (line 37)
         toBar1 = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPose), new Point(barCliponPose)))
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(180.001))
                 .build();
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        singleton = true;
+        pathTimer.resetTimer();
+        stateTimer.resetTimer();
+    }
+
+    public void autonomousUpdate() {
+        switch(pathState) {
+            case(0):
+                robot.outtake.setPivot(universalValues.OUTTAKE_COLLECT);
+                // TODO : fix intake pivot value on line 53 or make dedicated value in universalValues
+                robot.intake.setPivot(0.55);
+                follower.followPath(toBar1,true);
+                setPathState(1);
+            case(1):
+                if((follower.getPose().getX() > (barCliponPose.getX() - 1) && follower.getPose().getY() > (barCliponPose.getY() - 1))) {
+                    if (stateTimer.getElapsedTimeSeconds() > 3)
+                    {
+                        if (singleton){
+                            robot.outtake.setPivot(universalValues.OUTTAKE_CLIPON_UP);
+                            singleton = false;
+                        }
+                        if (stateTimer.getElapsedTimeSeconds() > 5) {
+                            robot.outtake.setPivot(universalValues.OUTTAKE_CLIPON_DOWN);
+                            if (stateTimer.getElapsedTimeSeconds() > 6)
+                            {
+                                setPathState(-1);
+                            }
+                        }
+                    }
+                }
+        }
     }
 
     @Override
     public void init() {
-        clipontimer = new Timer();
-        underbartimer = new Timer();
+
         robot = new robot(hardwareMap);
         follower = new Follower(hardwareMap);
+
+        stateTimer = new Timer();
+        pathTimer = new Timer();
+
         follower.setStartingPose(startPose);
+
+        robot.intake.setPivot(universalValues.INTAKE_INIT);
+        robot.intake.CloseIntake(universalValues.CLAW_CLOSE);
+        robot.outtake.setPivot(universalValues.OUTTAKE_COLLECT);
+        robot.outtake.CloseOuttake(universalValues.OUTTAKE_CLOSE);
+
         telemetry.update();
         buildPaths();
     }
 
     @Override
     public void loop() {
-            specimenonbar();
+
+        follower.update();
+        autonomousUpdate();
+
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+
     }
 
     @Override
     public void init_loop() {
-        underbartimer.resetTimer();
-        clipontimer.resetTimer();
+        stateTimer.resetTimer();
     }
 
     @Override
     public void start() {
-
+        setPathState(0);
     }
 
     @Override
